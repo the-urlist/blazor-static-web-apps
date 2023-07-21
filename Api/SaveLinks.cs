c8f0fad0fac3f5e6ddd668e33a98df11fe366346
Jon Galloway <jongalloway@gmail.com>
2023-07-21 09:11:02 -0700

    Add vanity URL checking

--- Api/SaveLinks.cs
+++ Api/SaveLinks.cs
@@ -5,14 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Api
 {
     public class SaveLinks
     {
+        protected const string CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        protected const string VANITY_REGEX = @"^([\w\d-])+(/([\w\d-])+)*$";
+
         private readonly ILogger _logger;
 
         public SaveLinks(ILoggerFactory loggerFactory)
@@ -24,7 +30,7 @@ namespace Api
         public async Task<SaveLinkResponse> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "links")] HttpRequestData req,
             FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("HttpExample");
+            var logger = executionContext.GetLogger("SaveLinks");
             logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var message = "Welcome to Azure Functions!";
@@ -42,6 +48,21 @@ namespace Api
                 };
             }
 
+            EnsureVanityUrl(linkBundle);
+
+            Match match = Regex.Match(linkBundle.VanityUrl, VANITY_REGEX, RegexOptions.IgnoreCase);
+
+            if (!match.Success)
+            {
+                // does not match
+                _logger.LogError("Invalid vanity url");
+                return new SaveLinkResponse()
+                {
+                    NewLinkBundle = null,
+                    HttpResponse = req.CreateResponse(HttpStatusCode.BadRequest)
+                };
+            }
+
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
             response.WriteString(message);
@@ -52,6 +73,28 @@ namespace Api
                 NewLinkBundle = linkBundle,
                 HttpResponse = response
             };
+        }
+
+        private void EnsureVanityUrl(LinkBundle linkDocument)
+        {
+            if (string.IsNullOrWhiteSpace(linkDocument.VanityUrl))
+            {
+                var code = new char[7];
+                var rng = new RNGCryptoServiceProvider();
+
+                var bytes = new byte[sizeof(uint)];
+                for (int i = 0; i < code.Length; i++)
+                {
+                    rng.GetBytes(bytes);
+                    uint num = BitConverter.ToUInt32(bytes, 0) % (uint)CHARACTERS.Length;
+                    code[i] = CHARACTERS[(int)num];
+                }
+
+                linkDocument.VanityUrl = new String(code);
+            }
+
+            // force lowercase
+            linkDocument.VanityUrl = linkDocument.VanityUrl.ToLower();
         }
 
         private static bool ValidatePayLoad(LinkBundle linkDocument, HttpRequestData req)
