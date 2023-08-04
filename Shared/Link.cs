7a9d276d824e31cebaf19cc2f4608ffe10418ace
Your Name <youremail@example.com>
2023-08-04 12:56:42 -0500

    form editing ouch

--- Shared/Link.cs
+++ Shared/Link.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazorApp.Shared
 {
     public class Link
     {
         public string Id { get; set; }
+
+        [Url]
         public string Url { get; set; }
+
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
