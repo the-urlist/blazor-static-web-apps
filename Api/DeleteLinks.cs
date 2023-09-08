977ae822aa6dd5a4dc8b3705abb8f045707a3f31
Jon Galloway <jongalloway@gmail.com>
2023-09-08 08:53:15 -0700

    Add Update and Delete methods

---
+++ Api/DeleteLinks.cs
@@ -0,0 +1,54 @@
+﻿using BlazorApp.Shared;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public class DeleteLinks
+    {
+        private readonly ILogger _logger;
+        private readonly CosmosClient _cosmosClient;
+
+        public DeleteLinks(ILoggerFactory loggerFactory, CosmosClient cosmosClient)
+        {
+            _logger = loggerFactory.CreateLogger<DeleteLinks>();
+            _cosmosClient = cosmosClient;
+        }
+
+        [Function("DeleteLinks")]
+        public async Task<HttpResponseData> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "links/{vanityUrl}")] HttpRequestData req,
+            string vanityUrl)
+        {
+            var container = _cosmosClient.GetContainer("TheUrlist", "linkbundles");
+
+            var link = container.GetItemLinqQueryable<LinkBundle>(true)
+                                 .Where(l => l.VanityUrl == vanityUrl)
+                                 .ToList()
+                                 .FirstOrDefault();
+
+            if (link == null)
+            {
+                var response = req.CreateResponse(System.Net.HttpStatusCode.NotFound);
+                await response.WriteStringAsync("Link not found");
+                return response;
+            }
+            else
+            {
+                var response = req.CreateResponse(System.Net.HttpStatusCode.OK);
+
+                await container.DeleteItemAsync<LinkBundle>(link.Id, new PartitionKey(link.VanityUrl));
+
+                await response.WriteStringAsync("Link deleted");
+                return response;
+            }
+        }
+    }
+}
