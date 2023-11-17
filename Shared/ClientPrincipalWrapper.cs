c3f2680826c1dfb548b459266fc1215fa68b4600
burkeholland <burkeholland@gmail.com>
2023-11-17 12:51:50 -0600

    Added user authentication and login modal. Updated navigation bar and layout. Created new CSS for login modal. Added new images and updated app settings.

---
+++ Shared/ClientPrincipalWrapper.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BlazorApp.Shared;
+
+public class ClientPrincipalWrapper
+{
+  public ClientPrincipal ClientPrincipal { get; set; } = new ClientPrincipal();
+}
---
+++ Shared/User.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BlazorApp.Shared;
+
+public class User
+{
+  public bool IsLoggedIn => ClientPrincipal != null && !string.IsNullOrEmpty(ClientPrincipal.UserId);
+  public ClientPrincipal ClientPrincipal { get; set; } = new ClientPrincipal();
+}
