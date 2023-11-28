4ba54eedf8f45ecec87185e835159c5f90921416
burkeholland <burkeholland@gmail.com>
2023-11-27 20:03:42 -0600

    Refactored JavaScript calls and added focus functionality in NewLink component

--- Client/wwwroot/js/main.js
+++ Client/wwwroot/js/main.js
@@ -1,9 +1,17 @@
-window.sortable = {
-  init: (dotnetObjRef, callback) => {
-    new Sortable(document.getElementById('linkBundle'), { 
-      handle: ".drag-handle",  // Drag handle selector within list items);
-      animation: 200,
-      forceFallback: true
-    });
+window.THEURLIST = {
+  sortable: {
+    init: (dotnetObjRef, callback) => {
+      new Sortable(document.getElementById('linkBundle'), { 
+        handle: ".drag-handle",  // Drag handle selector within list items);
+        animation: 200,
+        forceFallback: true
+      });
+    }
+  },
+  focusElement: (id) => {
+    setTimeout(() => {
+      const element = document.getElementById(id); 
+      element.focus();
+    }, 0);
   }
 }
