551ec45793a77264bbb497a489e3e49ffc8574a5
Burke Holland <burkeholland@gmail.com>
2024-01-31 13:06:28 -0600

    Update URL regular expression in Link.cs

--- Shared/Link.cs
+++ Shared/Link.cs
@@ -8,7 +8,7 @@ namespace BlazorApp.Shared
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        [RegularExpression(@"^(https?://)?([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?$", ErrorMessage = "That doesn't look like a valid URL"), Required]
+        [RegularExpression(@"^(https?://)?([\w-]+\.)+[\w-]+(/[\w- ./:?%&=#]*)?$", ErrorMessage = "That doesn't look like a valid URL"), Required]
         public string Url { get; set; }
 
         public string Title { get; set; }
