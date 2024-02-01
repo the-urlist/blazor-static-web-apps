e06a87461800f2be217e25dea34c4c7c064fb602
Jon Galloway <jongalloway@gmail.com>
2024-02-01 01:15:09 -0800

    Fixed auth for UpdateLinkBundle endpoint
    The most significant changes in the code are related to the interaction with the Cosmos DB container and the introduction of additional security checks. The code now retrieves the `ClientPrincipal` from the request and performs a query on the Cosmos DB container to find a matching `LinkBundle`. If a match is found, the code checks if the hashed username and the `IdentityProvider` match the `UserId` and `Provider` of the `LinkBundle`. If they do not match, an `Unauthorized` HTTP response is returned. If they do match, the `UserId` and `Provider` of the `LinkBundle` are updated and the `LinkBundle` is upserted into the Cosmos DB container. If no match is found, a `NotFound` HTTP response is returned.
    
    Changes:
    
    1. The `using` statements at the top of the file have been expanded to include additional namespaces such as `Api.Utility`, `BlazorApp.Shared`, `Microsoft.Azure.Cosmos`, `Microsoft.Azure.Functions.Worker`, `Microsoft.Azure.Functions.Worker.Http`, `Microsoft.Extensions.Logging`, `System`, and `System.Linq`.
    2. The constructor for the `UpdateLinkBundle` class has been modified to change the parameter name from `_cosmosClient` to `cosmosClient`.
    3. The code for interacting with the Cosmos DB container has been updated. It now retrieves the `ClientPrincipal` from the request and performs a query on the Cosmos DB container to find a matching `LinkBundle`.
    4. If a matching `LinkBundle` is found, the code checks if the hashed username and the `IdentityProvider` match the `UserId` and `Provider` of the `LinkBundle`. If they do not match, an `Unauthorized` HTTP response is returned.
    5. If the `UserId` and `Provider` do match, the `UserId` and `Provider` of the `LinkBundle` are updated and the `LinkBundle` is upserted into the Cosmos DB container.
    6. If no matching `LinkBundle` is found in the Cosmos DB container, then an HTTP response with a status code of `NotFound` is returned.

--- Api/Functions/UpdateLinkBundle.cs
+++ Api/Functions/UpdateLinkBundle.cs
@@ -1,15 +1,17 @@
+using Api.Utility;
 using BlazorApp.Shared;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
 namespace Api
 {
-    public class UpdateLinkBundle(CosmosClient _cosmosClient)
+    public class UpdateLinkBundle(CosmosClient cosmosClient)
     {
         [Function(nameof(UpdateLinkBundle))]
         public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "links/{vanityUrl}")] HttpRequestData req,
@@ -31,13 +33,36 @@ namespace Api
 
             try
             {
-                var container = _cosmosClient.GetContainer("TheUrlist", "linkbundles");
-                var partitionKey = new PartitionKey(vanityUrl);
-                var response = await container.UpsertItemAsync(linkBundle, partitionKey);
-                var responseMessage = req.CreateResponse(HttpStatusCode.OK);
-                var linkDocument = response.Resource;
-                await responseMessage.WriteAsJsonAsync(linkDocument);
-                return responseMessage;
+                ClientPrincipal principal = ClientPrincipalUtility.GetClientPrincipal(req);
+
+                var container = cosmosClient.GetContainer("TheUrlist", "linkbundles");
+
+                // get the document id where vanityUrl == vanityUrl
+                var query = new QueryDefinition("SELECT TOP 1 * FROM c WHERE c.vanityUrl = @vanityUrl")
+                    .WithParameter("@vanityUrl", vanityUrl);
+
+                var result = await container.GetItemQueryIterator<LinkBundle>(query).ReadNextAsync();
+
+                if (result.Count != 0)
+                {
+                    Hasher hasher = new();
+                    var hashedUsername = hasher.HashString(principal.UserDetails);
+                    if (hashedUsername != result.First().UserId || principal.IdentityProvider != result.First().Provider)
+                    {
+                        return await req.CreateJsonResponse(System.Net.HttpStatusCode.Unauthorized, message: "Unauthorized");
+                    }
+
+                    linkBundle.UserId = hashedUsername;
+                    linkBundle.Provider = principal.IdentityProvider;
+                    var partitionKey = new PartitionKey(vanityUrl);
+                    var response = await container.UpsertItemAsync(linkBundle, partitionKey);
+                    var responseMessage = req.CreateResponse(HttpStatusCode.OK);
+                    var linkDocument = response.Resource;
+                    await responseMessage.WriteAsJsonAsync(linkDocument);
+                    return responseMessage;
+                }
+
+                return await req.CreateJsonResponse(System.Net.HttpStatusCode.NotFound, message: "Link not found");
             }
             catch (Exception ex)
             {
