6508ec84c085eb6af0b2b147f094a4c21ee6d8b5
Burke Holland <burkeholland@gmail.com>
2024-04-10 14:21:10 -0500

    Refactor CSS styles in LoginButton, ModalWindow, ModalLogin, and app.css files

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,7 +11,7 @@ var environment = builder.HostEnvironment;
 var API_Prefix = environment.IsDevelopment() ? builder.Configuration["DEV_API_Prefix"] : builder.Configuration["PROD_API_Prefix"];
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(API_Prefix ?? builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddCascadingValue("API_Prefix", sp => API_Prefix ?? builder.HostEnvironment.BaseAddress);
 builder.Services.AddSingleton<StateContainer>();
 
 await builder.Build().RunAsync();
